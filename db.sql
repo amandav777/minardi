e229fcf116c6d4fe85c7599150f43f15baa05af8
Amanda Vieira <108702135+amandav777@users.noreply.github.com>
2023-11-27 19:37:40 -0300

    remove

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE colorfy;
-
-CREATE TABLE `colorfy`.`clientes` (
-  `idclientes` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(110) NOT NULL,
-  `telefone` VARCHAR(15) NOT NULL,
-  `senha` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`idclientes`));
